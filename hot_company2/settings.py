5783bb7ae00f95c34eeb89195790460078481fd6
Aldrich Huang <hpsoar@gmail.com>
2014-03-26 13:24:42 +0800

    initialized

---
+++ hot_company2/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for hot_company2 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'hot_company2'
+
+SPIDER_MODULES = ['hot_company2.spiders']
+NEWSPIDER_MODULE = 'hot_company2.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'hot_company2 (+http://www.yourdomain.com)'
